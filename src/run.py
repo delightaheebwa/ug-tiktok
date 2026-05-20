a84a2a3aca3fafd1c1b622ee999f6328d339cbff
delightaheebwa <delightaheebwa@users.noreply.github.com>
2026-05-21 00:36:26 +0300

    refactor fuzzy pipeline

--- src/run.py
+++ src/run.py
@@ -1,50 +1,39 @@
 from .steps import (
     normalize_text_unit,
-    load_words_to_set,
     identify_language,
     check_fuzzy_match,
 )
-from nltk.corpus import words as nltk_words
+from typing import Literal
 
 
 def run_steps(
-    initial_word: str,
-) -> tuple[str, tuple[str, str, float]] | list[str] | str:
+    initial_word: str, words_dict_set: dict[str, set[str]], words_dict_list: dict[str, list[str]]
+) -> tuple[str, tuple[str, float, int]] | list[str] | Literal["None"]:
     """Run the steps for sentiment analysis of the word in question"""
 
-    words_dict: dict[str, set[str]] = {
-        "English": set(nltk_words.words()),
-        "Luganda": load_words_to_set(
-            file_path="C:/Users/HP/Desktop/tiktok-analysis/words/processed/luganda_words.txt"
-        ),
-        "Swahili": load_words_to_set(
-            file_path="C:/Users/HP/Desktop/tiktok-analysis/words/processed/swahili_words.txt"
-        ),
-    }
-
+    
     normalized_word = normalize_text_unit(initial_word)
 
-    matches = identify_language(word=normalized_word, words=words_dict)
+    matches = identify_language(word=normalized_word, words=words_dict_set)
 
-    fuzzy_list = []
 
     if matches == ["Unknown"]:
         # since dictionary word wasn't found, fuzzy match is applied
-        for lang_name, lang_set in words_dict.items():
+        fuzzy_list = []
+        for lang_name, lang_set in words_dict_list.items():
             fuzzy_match = check_fuzzy_match(word=normalized_word, lang_set=lang_set)
-
             if fuzzy_match is not None:
                 fuzzy_list.append((lang_name, fuzzy_match))
-
-        # check if it is empty
+            
+        # check if list is empty
         if fuzzy_list:
-            # pick the tuple best fuzzy match  from the list of tuples
-            best_fuzzy_match = max(fuzzy_list, key=lambda x: x[1][2])
+            # max from list
+            best_fuzzy_word = max(fuzzy_list, key=lambda x: x[1][1])
+            return best_fuzzy_word
         else:
-            best_fuzzy_match = "None"
-
-        return best_fuzzy_match
-
+            best_fuzzy_word = "None"
+            return best_fuzzy_word
+        
     else:
         # dictionary word was found
         return matches
--- src/steps.py
+++ src/steps.py
@@ -1,5 +1,5 @@
 import re
-from rapidfuzz.process import cdist
+from rapidfuzz.process import extractOne
 from rapidfuzz import fuzz
 import numpy as np
 
@@ -26,18 +26,13 @@ def identify_language(word: str, words: dict[str, set[str]]) -> list[str]:
     return matches if matches else ["Unknown"]
 
 
-def check_fuzzy_match(word: str, lang_set: set[str]) -> tuple[str, str, float] | None:
+def check_fuzzy_match(word: str, lang_set: list[str]) -> tuple[str, float, int] | None:
     """Find a fuzzy match for a word"""
     # Only track matches that have an 80% similarity or higher
-    # for perfromance reasons and the fact that Iterable is what is accepted, we convert to list here
-    matrix = cdist([word], list(lang_set), scorer=fuzz.ratio, score_cutoff=80)
-
-    # Get the index of the highest score in the first row
-    best_idx = np.argmax(matrix[0])
-
-    # Check if the best score actually passes the threshold
-    if matrix[0][best_idx] > 0:
-        # (input word, matched word, similarity score)
-        return (word, list(lang_set)[best_idx], round(matrix[0][best_idx], 2))
+    best_fuzzy_tuple = extractOne(word, lang_set, scorer=fuzz.ratio, score_cutoff=80)
+    
+    if best_fuzzy_tuple is not None:
+        # (closest matching word, similarity score, position (index) of that match in the list.)
+        return best_fuzzy_tuple
     else:
         return None
